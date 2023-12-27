9339d084e6b347e08ccb6491ec41184d7cc8d23f
Hasan <h.mjabai92@gmail.com>
2023-12-27 11:07:18 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Code your solution here
+const words = ['spray', 'elite', 'exuberant', 'destruction', 'present'];
+
+const result = words.filter((word) => word.length > 6);
+
+console.log(result);
